69dbc2e9ac6665343d21746860e630ffdaf7cda9
GoodGuyGregory <greg.witt625@gmail.com>
2019-08-19 09:58:52 -0400

    adds first c++ kata solution

--- Cplusplus_Katas/can-we-divide.cpp
+++ Cplusplus_Katas/can-we-divide.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 /*  Kata:  
     Your task is to create function **isDivideBy** (or is_divide_by) 
     to check if an integer number is divisible by each out of two arguments.
@@ -19,5 +21,29 @@ EXAMPLES:
 bool isDivideBy(int number, int a, int b)
 {
   //your code here
-  
+  if ((number % a == 0) && (number % b == 0)) {
+    return true;
+  } 
+  else
+  {
+    return false;
+  } 
+
+}// test Method
+
+int main() {
+// Test Series One:
+std::cout <<"Series Test: (-12, 2, -6)\n";
+std::cout << "Result:" << isDivideBy(-12, 2, -6) << "\n";
+
+std::cout << " Series Test: (-12, 2, -5)\n";
+std::cout << "Result:" << isDivideBy(-12, 2, -5) << "\n";
+
+std::cout << " Series Test: (45, 1, 6)\n";
+std::cout << "Result:" << isDivideBy(45, 1, 6) << "\n";
+
+std::cout << " Series Test: (45, 5, 15)\n";
+std::cout << "Result:" << isDivideBy(45, 5, 15) << "\n";
+
 }
+;
